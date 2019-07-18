5799c0eb5b8242fe81c25669228f13fdd63cd9e4
Lupe <guadapenaranda@gmail.com>
2019-07-18 16:20:32 -0300

    Corregí el for de la funcion sacandole el = y el 1. Corregí el problema de segmentation fault allocando a y b. De lo contrario se pisan al momento de asignarles un lugar de memoria

--- debug/bugs/add_array_segfault.c
+++ debug/bugs/add_array_segfault.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-
+/*segmention fault: cuando intentas acceder a un lugar de memoria que no está permitido, por ejemplo pued eprovocar que dos variables se pisan*/
 int add_array(int *a, int *b, int n){
   int sum = 0;
   int i = 0;
-  for (i = 0; i <= n + 1; i++) {
+  for (i = 0; i < n; i++) {
     sum += abs(a[i]);
     sum += abs(b[i]);
   };
@@ -14,6 +14,8 @@ int main(int argc, char **argv) {
   int *a, *b;
   int n = 3;
   int i, sum;
+  a = malloc(sizeof(int) * n); /*aca les asigno un tamaño a a y b para que no se pisen, es el error que corregí*/
+  b = malloc(sizeof(int) * n);
   for (i = 0; i < n; i++) {
     a[i] = i;
     b[i] = i;
